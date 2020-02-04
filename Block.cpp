11ce72b7027ec115d7e5150e52a57e3bd6287a9a
Sky Noxt <skynoxt@gmail.com>
2020-02-04 21:50:27 +0100

    Slightly optimized block reading
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- Block.cpp
+++ Block.cpp
@@ -6,11 +6,9 @@ Block::Block(FILE* file)
 	fread(&header, sizeof(Header), 1, file);
 	data = new unsigned char[header.sampleSize];
 	fread(data, header.sampleSize, 1, file);
-	fseek(file, 5, SEEK_CUR);
 
-	unsigned char position = ftell(file) % 0x10;
-	if (position)
-		fseek(file, 0x10 - position, SEEK_CUR);
+	unsigned char position = (ftell(file) + 5) & 0x0f;
+	position ? fseek(file, 0x10 - position + 5, SEEK_CUR) : fseek(file, 5, SEEK_CUR);
 }
 
 Block::~Block()
